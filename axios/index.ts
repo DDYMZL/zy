3e00aa06da4e6559c0d5f386eaffd349811813ee
ziyou <874928519@qq.com>
2024-02-25 17:19:00 +0800

    feat: 初步封装

---
+++ axios/index.ts
@@ -0,0 +1,143 @@
+
+import axios from "axios";
+
+import type {
+  AxiosResponse,
+  AxiosInstance,
+  InternalAxiosRequestConfig,
+} from "axios";
+import type { IAuthConfig, IResponse } from './types/index.d'
+
+type TMethod =
+  | "POST"
+  | "OPTIONS"
+  | "GET"
+  | "HEAD"
+  | "PUT"
+  | "DELETE"
+  | "TRACE"
+  | "CONNECT";
+
+const TIME_OUT = 30000;
+
+const ERROE_CODE_MSG: { [key: number]: string } = {
+  401: "无效的会话，或者会话已过期，请重新登录！",
+  403: "当前操作没有权限",
+  404: "访问资源不存在",
+};
+
+interface IConfig {
+  url: string;
+  baseURL?: string;
+  method?: TMethod;
+  data?: any;
+  params?: any;
+  headers?: any;
+  timeout?: number;
+}
+
+class Request {
+  #instance: AxiosInstance;
+  authConfig: IAuthConfig;
+
+  constructor(config: IAuthConfig) {
+    this.authConfig = config;
+    this.#instance = axios.create({
+      baseURL: config.baseURL,
+      timeout: TIME_OUT,
+    });
+    this.#interceptor();
+  }
+
+  get<T = any, R = IResponse<T>>(config: IConfig) {
+    config.method = "GET";
+    return this.#instance<unknown, R>(config);
+  }
+
+  post<T = any, R = IResponse<T>>(config: IConfig) {
+    config.method = "POST";
+    return this.#instance<unknown, R>(config);
+  }
+
+  put<T = any, R = IResponse<T>>(config: IConfig) {
+    config.method = "PUT";
+    return this.#instance<unknown, R>(config);
+  }
+
+  delete<T = any, R = IResponse<T>>(config: IConfig) {
+    config.method = "DELETE";
+    return this.#instance<unknown, R>(config);
+  }
+
+  getDefault<T = any, R = IResponse<T>>(config: IConfig) {
+    config.method = "GET";
+    const instance = axios.create({
+      baseURL: "",
+      timeout: TIME_OUT,
+    });
+    this.#interceptorDefault(instance);
+    return instance<unknown, R>(config);
+  }
+
+  #interceptor() {
+    this.#instance.interceptors.request.use(
+      (config: InternalAxiosRequestConfig<any>) => {
+        if (!config.headers?.noToken) {
+          config.headers[this.authConfig.tokenKey] = this.authConfig.tokenStart + this.authConfig.getToken();
+        }
+        if (config.headers?.emptyBaseURL) {
+          config.baseURL = "";
+        }
+        return config;
+      },
+      (error) => {
+        return Promise.reject(error);
+      },
+    );
+
+    this.#instance.interceptors.response.use(
+      (response: AxiosResponse) => {
+        const { code, info } = response.data;
+        if (code === 200) {
+          return Promise.resolve(response.data);
+        } else if (info === "OK") {
+          return Promise.resolve(response.data);
+        } else {
+          return this.#handleError(response);
+        }
+      },
+      (error) => {
+        return Promise.reject(error);
+      },
+    );
+  }
+
+  #interceptorDefault(instance: AxiosInstance) {
+    instance.interceptors.response.use(
+      (response: AxiosResponse) => {
+        const { status } = response;
+        if (status === 200) {
+          return Promise.resolve(response.data);
+        } else {
+          return this.#handleError(response);
+        }
+      },
+      (error) => {
+        return Promise.reject(error);
+      },
+    );
+  }
+
+  #handleError(response: AxiosResponse) {
+    const { code, data, msg } = response.data;
+    const errmsg = ERROE_CODE_MSG[code] || msg;
+    this.authConfig.errorCb(errmsg)
+    if (code === 401) {
+      this.authConfig.removeToken();
+      location.href = this.authConfig.redirectUrl;
+    }
+    return Promise.reject(data);
+  }
+}
+
+export default Request;
---
+++ axios/types/index.d.ts
@@ -0,0 +1,93 @@
+/**
+ * 用户信息
+ *
+ * admin 是否为管理员
+ * nickName 用户昵称
+ * phonenumber 手机
+ * userName 用户名称
+ * avatar 头像
+ */
+export interface IUserInfo {
+  admin: boolean;
+  nickName: string;
+  phonenumber: string;
+  userName: string;
+  avatar: string;
+  userId: number;
+  dept: {
+    deptName: string;
+    deptId: number;
+  } | null;
+}
+
+export interface AxiosRequestConfig {
+  /** 隐藏报错提示 */
+  isHideMsg?: boolean;
+  /** 是否携带token */
+  isToken?: boolean;
+  /** 开启文件下载 */
+  downloadFile?: boolean;
+  /**token实效是否开启鉴权弹窗 */
+  isHideAuth?: boolean;
+}
+/**常规带分页响应数据结构 */
+export interface IResponse<T = any> {
+  code: number;
+  data: T;
+  msg: string;
+}
+/**常规带分页响应数据结构 */
+export interface IResponseTable<T = any> {
+  code: number;
+  data: T;
+  msg: string;
+  // 列表独有
+  rows: T[];
+  total: number;
+}
+/**验证码独有响应 */
+export interface ICaptchaResponse {
+  code: number;
+  msg: string;
+  captchaOnOff?: boolean;
+  img: string;
+  uuid: string;
+}
+/**登录独有响应 */
+export interface ILoginResponse {
+  code: number;
+  msg: string;
+  token: string;
+  jpassToken: string;
+}
+/**用户详情独有响应 */
+export interface IUserInfoResponse {
+  code: number;
+  msg: string;
+  permissions: any[];
+  /**权限列表 */
+  roles: string[];
+  /**用户信息 */
+  user: IUserInfo;
+}
+// 获取用户信息（包含岗位和角色分组）
+export interface IUserProfile {
+  code: number;
+  msg: string;
+  data: any;
+  postGroup: string;
+  roleGroup: string;
+}
+
+export interface IAuthConfig {
+  getToken: () => void;
+  removeToken: () => void;
+  errorCb: (msg: string) => void;
+  tokenStart: string;
+  tokenKey: string;
+  redirectUrl: string;
+  baseURL: string;
+}
+
+import axios from '../index'
+export default axios
